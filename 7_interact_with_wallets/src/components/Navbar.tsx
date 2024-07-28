415285e9886b5c78ffb71d95bbe543a7638ef094
RutamBhagat <rutambhagat@gmail.com>
2024-07-28 19:20:33 +0530

    add navbar tabs

--- 7_interact_with_wallets/src/components/Navbar.tsx
+++ 7_interact_with_wallets/src/components/Navbar.tsx
@@ -1,23 +1,18 @@
 "use client";
 
+import Link from "next/link";
+import { NavbarTabs } from "./NavbarTabs";
 import React from "react";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import dynamic from "next/dynamic";
-import { useWallet } from "@solana/wallet-adapter-react";
-
-const WalletMultiButtonDynamic = dynamic(async () => WalletMultiButton, { ssr: false });
 
 export default function NavBar() {
-  const wallet = useWallet();
-
   return (
     <div className="h-full flex-col flex bg-gray-900">
       <div className="container flex justify-between items-center py-4 h-16">
-        <h2 className="text-lg font-semibold text-white flex-shrink-0">Solana Wallet Interactions</h2>
+        <Link href={"/"} className="text-lg font-semibold text-white flex-shrink-0">
+          Solana Wallet Interactions
+        </Link>
         <div className="ml-auto flex items-center space-x-2">
-          <WalletMultiButtonDynamic>
-            {wallet.publicKey ? `${wallet.publicKey.toBase58().substring(0, 7)}...` : "Connect Wallet"}
-          </WalletMultiButtonDynamic>
+          <NavbarTabs />
         </div>
       </div>
     </div>
---
+++ 7_interact_with_wallets/src/components/NavbarTabs.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Link from "next/link";
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { cn } from "@/lib/utils";
+import dynamic from "next/dynamic";
+import { usePathname } from "next/navigation";
+import { useWallet } from "@solana/wallet-adapter-react";
+
+const examples = [
+  {
+    name: "Transfer",
+    href: "/",
+  },
+  {
+    name: "Ping",
+    href: "/ping",
+  },
+];
+
+interface ExamplesNavProps extends React.HTMLAttributes<HTMLDivElement> {}
+const WalletMultiButtonDynamic = dynamic(async () => WalletMultiButton, { ssr: false });
+
+export function NavbarTabs({ className, ...props }: ExamplesNavProps) {
+  const pathname = usePathname();
+  const wallet = useWallet();
+  return (
+    <div className="relative">
+      <div className={cn("flex items-center gap-x-4", className)} {...props}>
+        {examples.map((example, index) => (
+          <Link
+            href={example.href}
+            key={example.href}
+            className={cn(
+              "flex h-7 items-center justify-center rounded-full px-4 text-center text-sm transition-colors",
+              pathname === example.href ? "bg-muted font-medium text-primary" : "text-muted-foreground"
+            )}
+          >
+            {example.name}
+          </Link>
+        ))}
+        <div className="ml-8">
+          <WalletMultiButtonDynamic>
+            {wallet.publicKey ? `${wallet.publicKey.toBase58().substring(0, 7)}...` : "Connect Wallet"}
+          </WalletMultiButtonDynamic>
+        </div>
+      </div>
+    </div>
+  );
+}
